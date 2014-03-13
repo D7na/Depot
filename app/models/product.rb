293d87f649403405174149d807b062408e9c7298
D7na <8877024@gmail.com>
2014-03-13 10:49:27 +0400

    Add store controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
 validates :title, :description, :image_url, presence: true
 validates :price, numericality: {greater_than_or_equal_to: 1}
-validates :title, uniqueness: true
+validates :title, uniqueness: true, :length => {minimum: 10}
 validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)}i,
     message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
 }
+
 end
