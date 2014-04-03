b26757f09d6b97eea719123fe59292242645b38f
D7na <8877024@gmail.com>
2014-04-03 09:56:32 +0400

    Add email notification

--- config/application.rb
+++ config/application.rb
@@ -12,19 +12,41 @@ module Depot
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.initialize_on_precompile=false
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # Run "rake time:zones:all" for a list of tasks for finding time zone names. Default is UTC.
+    config.time_zone = 'Moscow'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = true
+    config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ru
+
+    Depot::Application.configure do
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:
+          "smtp.gmail.com",
+      port:
+          587,
+      domain:
+          "example.com",
+      authentication:
+          "plain",
+      user_name:
+          "8877024@gmail.com",
+      password:
+          "20185050309",
+      enable_starttls_auto: true
+  }
+    end
   end
 end
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
+
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
